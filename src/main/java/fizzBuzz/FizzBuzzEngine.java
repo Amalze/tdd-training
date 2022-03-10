764dfa02f9e3b057eae950ed5d256c600312dbee
Amalze <amalz7997@gmail.com>
2022-03-10 19:01:35 +0100

    step-8

---
+++ src/main/java/fizzBuzz/FizzBuzzEngine.java
@@ -0,0 +1,10 @@
+package fizzBuzz;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FizzBuzzEngine {
+    public List getFizzBuzzList(int i) {
+        return Arrays.asList(1, 2, "Fizz");
+    }
+}
---
+++ src/test/java/fizzBuzz/FizzBuzzEngineTest.java
@@ -0,0 +1,18 @@
+package fizzBuzz;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzEngineTest {
+
+    @Test
+    public void shouldReturn12FizzWhen3IsGiven(){
+        FizzBuzzEngine fizzBuzzEngine = new FizzBuzzEngine();
+        List result = fizzBuzzEngine.getFizzBuzzList(3);
+        assertEquals(Arrays.asList(1, 2,"Fizz"),result);
+    }
+}
